e5291992fa02691248ac518503dc7a3beadd05b0
john.perrow <john.perrow@c4plans.com>
2023-11-28 17:40:45 -0700

    squash
    update
    
    update
    
    asdf
    
    pudate
    
    asdf

--- preload/main.js
+++ preload/main.js
@@ -79,7 +79,7 @@ var balls = [];
 // define loop that keeps drawing the scene constantly
 
 function loop() {
-  ctx.fillStyle = 'rgba(0,0,0,0.25)';
+  ctx.fillStyle = 'rgba(100,100,100,0.25)';
   ctx.fillRect(0,0,width,height);
 
   while(balls.length < 25) {
